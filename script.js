b8be73856eddcb805eb30fd75514af1ab3ee8301
Men-ace <chhettrisailesh1@gmail.com>
2024-12-27 18:25:06 +1100

    making the AC  button work

--- script.js
+++ script.js
@@ -2,20 +2,37 @@
 
 
 
-const allButtonsElm = document.querySelectorAll('.btn');
-console.log(allButtonsElm);
+const allButtonsElm = document.querySelectorAll(".btn");
+
 
 let strToDisplay = "";
+const displayElm = document.querySelector(".display");
 
+ const buttonAction = (value) => {
+    console.log(value); 
+
+
+    if(value === "AC"){
+        strToDisplay = "";
+        return display(strToDisplay);
+    }
+
+    strToDisplay += value;
+
+    display(strToDisplay);
+ }
+
+// attaching click event to all the buttons. 
 allButtonsElm.forEach(
 (btn) => {
     btn.addEventListener("click", () =>{ 
         const value = btn.innerText; 
-            strToDisplay += value;
-
-            console.log(strToDisplay);
-      }   )
-}
+        buttonAction(value);
+      });
+    });
 
 
-);
+    //update clicked value button value to display area 
+    const display =  (str) => {
+        displayElm.innerText = str ||  "0.0" ;
+    };
