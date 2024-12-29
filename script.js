0335cb102b738aa85937544da0879046b1f9c193
Men-ace <chhettrisailesh1@gmail.com>
2024-12-29 15:11:10 +1100

    adding prank to the calcultor  by generating random value whem the conditon is that the value is more than 6

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const displayElm = document.querySelector(".display");
 
 const operators = ["+", "-", "/", "*", "%"];
 
+let  lastOperator = "";
 
  const buttonAction = (value) => {
     console.log(value); 
@@ -29,6 +30,8 @@ const operators = ["+", "-", "/", "*", "%"];
     }
 
     if(value === "=") {
+
+        lastOperator = "";
         // get the last character
          const lastChar = strToDisplay[strToDisplay.length - 1]; 
          //check if it is one of the opreators 
@@ -41,21 +44,42 @@ const operators = ["+", "-", "/", "*", "%"];
 
     // show only last clicked operator. 
     if (operators.includes(value)){
+
+        lastOperator = value; 
+
         //get the last character 
         const lastChar =  strToDisplay[strToDisplay.length - 1];
-
+       
+        //check if the is one of the operators. 
         if (operators.includes(lastChar)){
             strToDisplay = strToDisplay.slice(0,-1);
         }
+
     }
 
 
+    // handle the dot ciclk
+    if (value === ". ") {
+        const lastOperatorIndex =  strToDisplay.
+        lastIndexOf(lastOperator);
 
+        const lastNumberSet = strToDisplay.slice(lastOperatorIndex);
+
+        if (lastNumberSet.includes(".")){
+            return;
+        }
+
+        if( !lastOperator && strToDisplay.includes(".")){
+            return; 
+        }
+    }
 
     strToDisplay += value;
 
     display(strToDisplay);
- }
+ };
+
+
 
 // attaching click event to all the buttons. 
 allButtonsElm.forEach(
@@ -75,8 +99,19 @@ allButtonsElm.forEach(
 
     // calculate total 
     const displayTotal  = () => {
-         const total =  eval(strToDisplay);
+
+        const extraValue = randomValue();
+
+
+         const total =  eval(strToDisplay) + extraValue ;
 
          strToDisplay = total.toString();
          display(strToDisplay);
     };
+
+
+    const randomValue = () => {
+        const num = Math.round(Math.random() * 10);
+        return num < 9? num : 0;
+     };
+    
